5bfae50d1f0d8a2fca0f2a68462265a0f03093e1
tirthagautam <122071829+tirthagautam05@users.noreply.github.com>
2023-01-09 23:28:27 +0000

    Some bug changes

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Elite101-PreWork
-A chatbot
-# new branch
+This is a simple chatbot program. When run, it will ask for the user's name and then engage in conversation with them. The user can ask the chatbot questions or make statements and the chatbot will respond with a variety of pre-determined responses. The user can also type "bye" at any time to end the conversation. 
+# new branch 
 this is the change in new branch.
 add more options to what bot says
+The chatbot is programmed to respond to specific phrases, such as "what are your store hours" and "are you open today?", with pre-determined responses. The chatbot will also randomly select from a list of responses for any other input from the user.
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import random
+
+
 def chatbot():
-  print ("Hello, I'm chatbot. what is your name?")
+  print("Hello, I'm chatbot. what is your name?")
   name = input()
-  print(f"Nice to meet you, {name}! What can I help you with today? you can type \"bye\" to leave me :(?")
+  print(
+    f"Nice to meet you, {name}! What can I help you with today? you can type \"bye\" to leave me :(?"
+  )
 
   while True:
-    message = input ()
-    answers = ["Can you repeat that again", "You could be right.", "I hear you.", "I can see where you are coming from.", "Ah-ha.", "That could be.", "I don't know; what do you think?"]  # could add more answers. here to be personlized 
+    message = input()
+    answers = [
+      "Can you repeat that again", "You could be right.", "I hear you.",
+      "I can see where you are coming from.", "Ah-ha.", "That could be.",
+      "I don't know; what do you think?"
+    ]  # could add more answers. here to be personlized
     if message == "bye":
       print("Goodbye! Have a great day.")
       break
@@ -19,7 +27,6 @@ def chatbot():
     else:
       response = random.choice(answers)
       print(response)
-      
-      
+
+
 chatbot()
-  
